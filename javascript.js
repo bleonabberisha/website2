ae2b60ffa62811ad7af582e80f929d1c5252bc83
Bleona <blbe0001@stud.kea.dk>
2024-06-08 10:38:18 +0200

    burger menu ændringer

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
-document.querySelector('.burger-menu').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('show');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 });
